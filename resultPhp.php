a1f06b1805b617ae43b1eac409ec89ed863fa4ff
Baazza-Salah <baazzasalaheddine@gmail.com>
2023-12-16 18:59:56 +0100

    vol11

--- resultPhp.php
+++ resultPhp.php
@@ -2,9 +2,9 @@
 
 // Connect to your database (replace with your actual database credentials)
 $servername = "localhost";
-$username = "your_username";
-$password = "your_password";
-$dbname = "your_database";
+$username = "root";
+$password = "";
+$dbname = "p1";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
@@ -14,70 +14,65 @@ if ($conn->connect_error) {
 }
 
 // Set default filter
-$filter = "new";
-
-// Change filter based on user selection
-if (isset($_GET['filter'])) {
-    $filter = $_GET['filter'];
-}
-
 // Set default keywords
-$keywords = [];
-
-// Change keywords based on user input
-if (isset($_GET['keywords'])) {
-    // Split the input string into an array of keywords
-    $keywords = preg_split("/[\s,;]+/", $_GET['keywords']);
-}
+$keywords = isset($_GET['keywords']) ? preg_split("/[\s,;]+/", $_GET['keywords']) : [];
 
 // Set default salary range
 $minSalary = isset($_GET['min_salary']) ? $_GET['min_salary'] : 0;
 $maxSalary = isset($_GET['max_salary']) ? $_GET['max_salary'] : PHP_INT_MAX;
 
-// Fetch jobs based on the selected filter, keywords, and salary range
 $sql = "";
+$orderBy = " ORDER BY j.posted_time DESC"; // Default order
 
-switch ($filter) {
-    case "high_salary":
-        $sql = "SELECT j.*, c.company_name, c.company_picture 
-                FROM jobs_table j, company c
-                WHERE j.id_company = c.id_company
-                AND j.salary BETWEEN $minSalary AND $maxSalary
-                " . buildKeywordFilter($keywords) . "
-                ORDER BY j.salary DESC";
-        break;
-    case "full_time":
-        $sql = "SELECT j.*, c.company_name, c.company_picture 
-                FROM jobs_table j, company c
-                WHERE j.id_company = c.id_company
-                AND j.full_time = 'Full Time'
-                AND j.salary BETWEEN $minSalary AND $maxSalary
-                " . buildKeywordFilter($keywords);
-        break;
-    case "part_time":
-        $sql = "SELECT j.*, c.company_name, c.company_picture 
-                FROM jobs_table j, company c
-                WHERE j.id_company = c.id_company
-                AND j.full_time = 'Part Time'
-                AND j.salary BETWEEN $minSalary AND $maxSalary
-                " . buildKeywordFilter($keywords);
-        break;
-    case "new":
-    default:
-        $sql = "SELECT j.*, c.company_name, c.company_picture 
-                FROM jobs_table j, company c
-                WHERE j.id_company = c.id_company
-                AND j.salary BETWEEN $minSalary AND $maxSalary
-                " . buildKeywordFilter($keywords) . "
-                ORDER BY j.posted_time DESC";
-        break;
+$commonQuery = "SELECT j.*, c.company_name, c.company_picture 
+               FROM jobs_table j
+               INNER JOIN company c ON j.id_company = c.id_company
+               WHERE j.salary BETWEEN $minSalary AND $maxSalary";
+
+if (isset($_GET['filter'])) {
+    switch ($_GET['filter']) {
+        case "high_salary":
+            $orderBy = " ORDER BY j.salary DESC";
+            break;
+        case "full_time":
+            $commonQuery .= (strpos($commonQuery, 'WHERE') !== false ? " AND" : " WHERE") . " j.full_time = 'Full Time'";
+            break;
+        case "part_time":
+            $commonQuery .= (strpos($commonQuery, 'WHERE') !== false ? " AND" : " WHERE") . " j.full_time = 'Part Time'";
+            break;
+        // Add additional cases as needed
+    }
 }
 
+$sql = $commonQuery . $orderBy;
 $result = $conn->query($sql);
 
-// Display each job as a Bootstrap card
+
+
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
+        // Calculate the time difference
+        $postedTime = new DateTime($row["posted_time"]);
+        $currentDateTime = new DateTime();
+        $timeDifference = $currentDateTime->diff($postedTime);
+
+        // Format the elapsed time
+        $elapsedTime = '';
+        if ($timeDifference->y > 0) {
+            $elapsedTime = $timeDifference->format('%y years ago');
+        } elseif ($timeDifference->m > 0) {
+            $elapsedTime = $timeDifference->format('%m months ago');
+        } elseif ($timeDifference->d > 0) {
+            $elapsedTime = $timeDifference->format('%d days ago');
+        } elseif ($timeDifference->h > 0) {
+            $elapsedTime = $timeDifference->format('%h hours ago');
+        } elseif ($timeDifference->i > 0) {
+            $elapsedTime = $timeDifference->format('%i minutes ago');
+        } else {
+            $elapsedTime = 'Just now';
+        }
+
+        // Output the job details as a Bootstrap card
         echo '
         <a href="jobInformation.html?id=' . $row["job_id"] . '" class="card m-md-5 p-1 job-card">
             <div class="row no-gutters">
@@ -93,7 +88,7 @@ if ($result->num_rows > 0) {
                             <h5 class="card-title">' . $row["job_name"] . '<span class="badge rounded-pill ' . ($row["full_time"] == "Full Time" ? 'bg-success' : 'bg-warning') . '">' . $row["full_time"] . '</span></h5>
                             <h6 class="card-subtitle mb-2 text-muted">' . $row["company_name"] . '</h6>
                             <!-- Part Time or Full Time Badge -->
-                            <p class="card-text">' . $row["salary"] . $row["jobType"] . '</p>
+                            <p class="card-text">' . $row["salary"] . '</p>
                         </div>
                         <!-- Center Column for Apply Button -->
                         <div class="d-flex justify-content-between align-items-center col-md-2">
@@ -101,15 +96,15 @@ if ($result->num_rows > 0) {
                         </div>
                         <!-- Right Column for Posted Time -->
                         <div class="d-flex justify-content-between align-items-center col-md-3">
-                            <small class="text-muted">Posted ' . $row["posted_time"] . ' ago</small>
+                            <small class="text-muted">Posted ' . $elapsedTime . '</small>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
         </a>';
     }
 } else {
+    // Display a message if no jobs are found
     echo '<p>No jobs found.</p>';
 }
 
@@ -123,15 +118,20 @@ $conn->close();
  * @return string
  */
 function buildKeywordFilter($keywords) {
-    if (empty($keywords)) {
-        return ""; // Return an empty string if no keywords are provided
-    }
-
     $keywordConditions = [];
+
     foreach ($keywords as $keyword) {
         $keywordConditions[] = "j.job_name LIKE '%$keyword%'";
     }
 
-    return " AND (" . implode(" OR ", $keywordConditions) . ")";
+    // Check if any keyword conditions are generated
+    if (!empty($keywordConditions)) {
+        return " AND (" . implode(" OR ", $keywordConditions) . ")";
+    }
+
+    // Return an empty string if no keywords are provided
+    return "";
 }
+
+
 ?>
